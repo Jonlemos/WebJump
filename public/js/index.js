204ce0d1c982d0b1498e85e7038584e44de2cd54
Jonlemos <lemos.anjos@gmail.com>
2020-05-23 20:56:38 -0300

    Adicionado menu mobile

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,19 @@
     
     var shoesShop = document.getElementById('shoes-shop')
     var linkShop = document.getElementById('link-shop')
+    var menuMobile = document.getElementById('menu-mobile')
     var exactProduct = document.getElementById('exactProduct')
     
+    menuMobile.addEventListener('click', ClickmenuMobile)
+    function ClickmenuMobile(){
+        var menuClick = document.getElementById('menu-acess')
+
+        if (menuClick.style.display === "flex") {
+            menuClick.style.display = "none";
+          } else {
+            menuClick.style.display = "flex";
+        }
+    }
     var listProduct
     axios.get('http://localhost:8888/api/V1/categories/list').then(function (response) {
             
